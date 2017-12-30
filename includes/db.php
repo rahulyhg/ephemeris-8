3aa0834c1343f6d64055a5bd17e8968cb8fe63bd
Mohamed Alsoudani <mr.hassuny@gmail.com>
2017-12-30 13:39:38 -0700

    adding html and css

---
+++ includes/db.php
@@ -0,0 +1,14 @@
+<?php
+    $db['host'] = 'localhost';
+    $db['username'] = 'root';
+    $db['password'] = '';
+    $db['name'] = 'cms';
+
+    foreach($db as $key => $value){
+        define(strtoupper($key), $value);
+    }
+    $connection = mysqli_connect(HOST, USERNAME, PASSWORD, NAME);
+    if ($connection){
+        echo "we are connected";
+    }
+?>
